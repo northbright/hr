868ecf3136f179cf7aebda726cb0ca2e1cb7ea99
Frank Xu <frank.xu@northbright.com>
2018-05-09 16:28:56 +0800

    Add .vue files

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <group title="登录">
+      <x-input name="username" placeholder="用户名／手机）" v-model="username"></x-input>
+      <x-input name="password" placeholder="请输入密码" type="password" v-model="password"></x-input>
+      <x-button type="primary" v-on:click.native="login">OK</x-button>
+    </group>
+  </div>
+</template>
+
+<script>
+import { XInput, Group, XButton, Cell } from 'vux'
+import * as types from '../store/types'
+
+export default {
+  components: {
+    XInput,
+    XButton,
+    Group,
+    Cell
+  },
+  data () {
+    return {
+	username: "",
+	password: "",
+        csrf_token: ""
+    }
+  },
+  methods: {
+    mounted() {
+       console.log("mounted().")
+    },
+    login() {
+       console.log("login().") 
+
+       this.axios.get('/csrf_token').then( (response) => {
+         console.log("csrf token: " + response.data.csrf_token)
+         this.csrf_token = response.data.csrf_token
+
+         this.axios.post('/login', {
+           username: this.username,
+           password: this.password,
+           csrf_token: this.csrf_token
+         })
+         .then( (response) => {
+           console.log(response)
+           this.$store.commit(types.LOGIN, response.id)
+           this.$router.push({
+             path: '/',
+           })
+         })
+         .catch( (error) =>  {
+           console.log("POST /login error: " + error)
+         })
+       })
+       .catch( (error) => {
+           console.log("GET /login error:" + error)
+       })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+.green {
+  color: green;
+}
+</style>
+
---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <msg :title="title" :description="description" :buttons="buttons" :icon="icon"></msg>
+  </div>
+</template>
+
+<script>
+import { Msg, Divider, XButton } from 'vux'
+import * as types from '../store/types'
+
+export default {
+  components: {
+    Msg,
+    Divider,
+    XButton,
+  },
+  data () {
+    return {
+	title: 'Page not found',
+	description: 'Current page is not found.',
+        icon: 'warn',
+        buttons: [{
+          type: 'primary',
+          text: 'Go to Home',
+          link: '/'
+        }]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+.green {
+  color: green;
+}
+</style>
+
